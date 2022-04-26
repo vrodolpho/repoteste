957783beb302a66c95201c7042133e722ea11774
vrodolpho <61160164+vrodolpho@users.noreply.github.com>
2022-04-26 01:45:14 +0000

    Soma dos números.

---
+++ soma.py
@@ -0,0 +1,7 @@
+# Definição das variáveis
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+print(f'A soma entre os números é {n1+n2}, ')
+print(n1 / n2)
+print(n1 + n2)
