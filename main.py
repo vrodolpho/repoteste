f846fafc8b25aded257157c303c120eb633f6b6d
vrodolpho <61160164+vrodolpho@users.noreply.github.com>
2022-04-26 04:00:08 +0000

    Criar formas geométricas.

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
 import turtle
-
-s = turtle.Screen()
-t = turtle.Turtle()
-turtle.shape("turtle")
-
-print('Para onde vamos?')
-# se movendo
-x = float(input('Digite o valor x\n'))
-y = float(input('Digite o valor y\n'))
-t.goto(x, y)
-
-print('Vamos girar!')
-angulo = int(input('Digite o valor do angulo\n'))
-t.left(angulo)
+open = turtle.Pen()
+pen.forward(50)
+pen.right(45)
+pen.forward(50)
